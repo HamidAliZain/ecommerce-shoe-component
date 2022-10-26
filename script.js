8ca9b6cbbfa0a945c1326e842a3dec8abf9132b0
HAmid <hamidprince823@gmail.com>
2022-10-26 11:59:10 +0500

    completed😊

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function increment() {
   document.getElementById("result").innerHTML = "$" + incre * 125.0;
   document.getElementById("bage").innerText = incre;
 }
+// Minus //
 function decrement() {
   if (inputVal.value > 0) {
     inputVal.value--;
@@ -28,11 +29,15 @@ function cartSection() {
   }
 }
 // add items in cart section //
-function addItems(e) {
+function addItems() {
   if (inputVal.value == 0) {
-    alert("d");
+    document.getElementById("alert").innerHTML = "Select atleast one item ";
+    setTimeout(function alert() {
+      document.getElementById("alert").innerHTML = "Add to Cart";
+    }, 2000);
   } else {
     document.getElementById("Cartempty").style.display = "none";
+    document.getElementById("alert").innerHTML = "Added to Cart";
     document.getElementById("AddCartItem").className = "addCartitem";
     document.getElementById("cartOrder").style.display = "flex";
     document.getElementById("bage").style.display = "block";
@@ -40,9 +45,17 @@ function addItems(e) {
 }
 // checkOut //
 function checkout() {
+  
+  document.getElementById("alert").innerHTML = "Add to Cart";
   document.getElementById("AddCartItem").className = "checkOut";
   document.getElementById("cartOrder").style.display = "none";
   document.getElementById("Cartempty").style.display = "flex";
   document.getElementById("bage").style.display = "none";
-    inputVal.value = 0;
+  inputVal.value = 0;
 }
+ 
+
+let bgColor = ["#ffffff", "#ffffff"];
+document.querySelectorAll(".swiper-button").forEach(function (d, i) {
+  d.style.color = bgColor[i];
+});
